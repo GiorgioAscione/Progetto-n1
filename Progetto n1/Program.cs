a656b47d506d2940b0f2d555f72a3b801a1e85e0
GiorgioAscione <acergio95@libero.it>
2022-03-31 12:11:40 +0200

    Add project files.

---
+++ Progetto n1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Aggiungi un nome");
+
+Console.ReadLine();
+
+var testo = Console.ReadLine();
+
+Console.WriteLine(testo);
